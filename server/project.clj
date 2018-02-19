0bc461e7af1da6318493314ecd47c8212c47baf8
Canna Wen <cannawen@gmail.com>
2018-02-19 21:23:46 +0800

    Create RSS feed of audio

--- server/project.clj
+++ server/project.clj
@@ -10,7 +10,8 @@
                  [ring/ring-codec "1.1.0"]
                  [hiccup "1.0.5"]
                  [io.forward/yaml "1.0.6"]
-                 [clj-time "0.14.2"]]
+                 [clj-time "0.14.2"]
+                 [clj-rss "0.2.3"]]
   :main ^:skip-aot server.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- server/src/server/routes/api.clj
+++ server/src/server/routes/api.clj
@@ -9,30 +9,56 @@
     [clj-time.coerce :as c]
     [clojure.java.shell :refer [sh with-sh-dir]]
     [org.httpkit.client :as http]
-    [cheshire.core :as json]))
+    [cheshire.core :as json]
+    [clj-rss.core :as rss]))
 
-(defn save-to-file [fname content]
-  (with-open [o (io/output-stream fname)]
+(defn save-to-file [file-name content]
+  (with-open [o (io/output-stream file-name)]
     (.write o content)))
 
-(defn save-chapter! [{:keys [text]}]
-
+(defn save-chapter! [title text]
   (->> @(http/request {:url "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize"
                        :method :post
                        :basic-auth ["7c0a9e4c-e80f-45d5-bb2e-8eb9cfffbf55"
-                                    "qtzLNyofVSzK"]
+                                    "qtzLNyofVSzK"]         ;TODO revoke credentials
                        :headers {"Content-Type" "application/json"
                                  "Accept" "audio/mp3"}
                        :as :byte-array
                        :body (json/encode {"text" text})})
-      :body
-      (save-to-file "media-out/test.mp3")))
+    :body
+    (save-to-file (str "public/media/" title ".mp3"))))
+
+(defn get-files [directory extension]
+  (->> directory
+    clojure.java.io/file
+    file-seq
+    (filter (fn [file]
+              (clojure.string/ends-with? (.getPath file) extension)))
+    (map (fn [file] (.getName file)))))
+
+(defn generate-rss-item [file-name]
+  {:title file-name :link (str "http://localhost:2351/" file-name)})
+
+(defn generate-rss! []
+  (let [description {:title "Generated RSS Feed" :link "http://localhost:2351/" :description "Watson-generated RSS Feed"}
+        items (map generate-rss-item (get-files "public/media" "mp3"))
+        rss (apply rss/channel-xml description items)]
+    (spit "public/rss.xml" rss)))
 
 (defroutes routes
   (context "/api" _
 
-    (POST "/chapter" request
-      (save-chapter! (:body request))
-      (println "chapter processed")
+    (POST "/generate-rss" request
+      (generate-rss!)
+      (println "generated rss")
       {:status 200
-       :body "OK"})))
+       :body "OK"})
+
+    (POST "/chapter" request
+      (let [title (get-in request :body :title)
+            text (get-in request :body :text)]
+        (save-chapter! title text)
+        (generate-rss!)
+        (println "chapter processed")
+        {:status 200
+         :body "OK"}))))
